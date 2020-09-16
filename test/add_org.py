be5ed5fb220b162f1d69562d389285b1a1fb6806
ThorodanBrom <bryan.robert@datakaveri.org>
2020-09-16 11:47:03 +0000

    Update provider access tests to use new APIs

--- test/add_org.py
+++ test/add_org.py
@@ -16,11 +16,19 @@ cursor = conn.cursor()
 def add_organization(website):
 
         try:
-                cursor.execute("insert into consent.organizations (name,website,city,state,country,created_at,updated_at) values ('Testing Org', '" + website + "', 'testing-city', 'TA', 'TS', now(), now()) returning id")
+                cursor.execute("select id from consent.organizations where website = '" + website + "'")
                 conn.commit()
 
         except psycopg2.DatabaseError as error:
                 return {}
+ 
+        if cursor.rowcount == 0:
+                try:
+                        cursor.execute("insert into consent.organizations (name,website,city,state,country,created_at,updated_at) values ('Testing Org', '" + website + "', 'testing-city', 'TA', 'TS', now(), now()) returning id")
+                        conn.commit()
+
+                except psycopg2.DatabaseError as error:
+                        return {}
 
         oid = cursor.fetchone()[0]
         return oid
