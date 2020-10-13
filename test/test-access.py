c6187ad936cedefe7b10a13a8c97ad6f3cc6c536
ThorodanBrom <bryan.robert@datakaveri.org>
2020-10-13 12:33:16 +0000

    Added delete access rules API * request is array of objects, each containing access ID * if need to delete individual capability, add array of caps to   be deleted to the object * Added policy text generation function (TODO, refactor POST API to use this) * Updated tests

--- test/test-access.py
+++ test/test-access.py
@@ -115,8 +115,8 @@ r = untrusted.provider_access([req])
 assert r['success']     == True
 assert r['status_code'] == 200
 
-apis = ["/ngsi-ld/v1/entityOperations/query", "/ngsi-ld/v1/entities", "/ngsi-ld/v1/temporal/entities","/ngsi-ld/v1/entities/" + resource_id, "/ngsi-ld/v1/subscription"]
-body = {"id" : resource_id + "/someitem", "apis" : apis }
+apis = ["/ngsi-ld/v1/entityOperations/query", "/ngsi-ld/v1/entities", "/ngsi-ld/v1/temporal/entities","/ngsi-ld/v1/entities/" + provider_id + '/rs.example.co.in/' + resource_group, "/ngsi-ld/v1/subscription"]
+body = {"id" : provider_id + '/rs.example.co.in/' + resource_group + "/someitem", "apis" : apis }
 r = consumer.get_token(body)
 assert r['success']     is True
 
@@ -159,8 +159,8 @@ assert r['status_code'] == 403
 
 ##### data ingester #####
 
-resource_group = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
-resource_id = provider_id + "/rs.example.com/" + resource_group
+diresource_group = ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
+diresource_id = provider_id + "/rs.example.com/" + diresource_group
 
 body        = {"id" : resource_id + "/someitem", "api" : "/iudx/v1/adapter" }
 
@@ -174,7 +174,7 @@ assert r['status_code'] == 200
 
 # invalid resource type
 req["user_role"]    = "data ingester"
-req["item_id"]      = resource_id
+req["item_id"]      = diresource_id
 req["item_type"]    = "catalogue"
 r = untrusted.provider_access([req])
 assert r['success']     == False
@@ -186,7 +186,7 @@ assert r['success']     == True
 assert r['status_code'] == 200
 
 # without adapter API
-body = {"id"    : resource_id + "/*" }
+body = {"id"    : diresource_id + "/*" }
 
 r = consumer.get_token(body)
 assert r['success']     is False
@@ -196,7 +196,7 @@ r = consumer.get_token(body)
 assert r['success']     is True
 
 # request for other items in resource group
-body = {"id" : resource_id + "/someitem/someotheritem", "api" : "/iudx/v1/adapter" }
+body = {"id" : diresource_id + "/someitem/someotheritem", "api" : "/iudx/v1/adapter" }
 r = consumer.get_token(body)
 assert r['success']     is True
 
@@ -217,13 +217,90 @@ assert r['success']     == True
 assert r['status_code'] == 200
 rules = r['response']
 for r in rules:
-        if r['email'] == email and r['role'] == 'consumer':
+        if r['email'] == email and r['role'] == 'consumer' and resource_id == r['item']['cat_id']:
+                consumer_id = r['id']
                 assert set(r['capabilities']).issubset(set(['temporal', 'subscription', 'complex']))
                 assert len(r['capabilities']) <= 3 and len(r['capabilities']) >= 1
         if r['email'] == email and r['role'] == 'onboarder':
+                onboarder_id = r['id']
                 assert r['item_type'] == 'catalogue'
-        if r['email'] == email and r['role'] == 'data ingester':
+        if r['email'] == email and r['role'] == 'data ingester' and diresource_id == r['item']['cat_id']:
+                ingester_id = r['id']
                 assert r['policy'].endswith('"/iudx/v1/adapter"')
+
+### deleting rules ###
+
+token_body = { "id"    : provider_id + "/catalogue.iudx.io/catalogue/crud" }
+
+r = consumer.get_token(token_body)
+assert r['success']     is True
+assert None != r['response']['token']
+
+body = {"id" : onboarder_id}
+r = untrusted.delete_rule([body])
+assert r['success']     == True
+assert r['status_code'] == 200
+
+# onboarder token request should fail
+r = consumer.get_token(token_body)
+assert r['success']     is False
+
+# 
+token_body = {"id" : diresource_id + "/someitem/someotheritem", "api" : "/iudx/v1/adapter" }
+r = consumer.get_token(token_body)
+assert r['success']     is True
+
+token_body = {"id" : resource_id + "/something", "apis" : ["/ngsi-ld/v1/temporal/entities"] }
+r = consumer.get_token(token_body)
+assert r['success']     is True
+
+body = [{"id": ingester_id}, {"id": consumer_id, "capability": ["temporal"]}]
+r = untrusted.delete_rule(body)
+assert r['success']     == True
+assert r['status_code'] == 200
+
+body = [{"id": ingester_id}, {"id": consumer_id, "capability": ["temporal"]}]
+r = untrusted.delete_rule(body)
+assert r['success']     == False
+assert r['status_code'] == 403
+
+token_body = {"id" : diresource_id + "/someitem/someotheritem", "api" : "/iudx/v1/adapter" }
+r = consumer.get_token(token_body)
+assert r['success']     is False
+
+token_body = {"id" : resource_id + "/something", "apis" : ["/ngsi-ld/v1/temporal/entities"] }
+r = consumer.get_token(token_body)
+assert r['success']     is False
+
+# get token for subscription, complex
+apis = ["/ngsi-ld/v1/entityOperations/query", "/ngsi-ld/v1/entities","/ngsi-ld/v1/entities/" +  resource_id, "/ngsi-ld/v1/subscription"]
+token_body = {"id" : resource_id + "/someitem", "apis" : apis }
+r = consumer.get_token(token_body)
+assert r['success']     is True
+
+# temporal not there
+body = [{"id": consumer_id, "capability": ["temporal", "subscription", "complex"]}]
+r = untrusted.delete_rule(body)
+assert r['success']     == False
+assert r['status_code'] == 403
+
+token_body = {"id" : resource_id + "/someitem", "apis" : apis }
+r = consumer.get_token(token_body)
+assert r['success']     is True
+
+# delete full rule
+body = [{"id": consumer_id}]
+r = untrusted.delete_rule(body)
+assert r['success']     == True
+assert r['status_code'] == 200
+
+token_body = {"id" : resource_id + "/someitem", "apis" : apis }
+r = consumer.get_token(token_body)
+assert r['success']     is False
+
+token_body = {"id" : resource_id + "/someitem", "apis" : ["/ngsi-ld/v1/subscription"] }
+r = consumer.get_token(token_body)
+assert r['success']     is False
 
 ### setting multiple rules ###
 
